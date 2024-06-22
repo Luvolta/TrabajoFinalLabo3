f3b7c67e9ad1e982944fc416623e6b160730b1f9
Luvolta <luvolta99@gmail.com>
2024-06-22 19:55:48 -0300

    Implementación API

--- main.js
+++ main.js
@@ -34,4 +34,64 @@ setTimeout(() => {
     slides[currentSlide].classList.add('active');
 }, 20);
 
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('.menu').classList.add('active');
+});
+
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('body').classList.add('bloqueo');
+});
+
+document.querySelector('.closeMenu-icon').addEventListener('click', function() {
+    document.querySelector('.menu').classList.remove('active');
+});
+
+
+//API
+const apiKey = "c917c32c98164da594fa9b1655e69d07";
+const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}`;
+
+async function getGames() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  displayGames(data.results);
+}
+
+function displayGames(games) {
+  const gameContainer = document.getElementById("game-container");
+  games.forEach((game) => {
+    const gameCard = document.createElement("div");
+
+    gameCard.innerHTML = `<img src="${game.background_image}" alt="${game.name}"> <h3>${game.name}</h3>`;
+
+    gameContainer.appendChild(gameCard);
+    console.log(game);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", getGames);
+
+//  document.getElementById('search-input').addEventListener('input', function() {
+//      const query = this.value.trim();
+//      if (query.length > 2) {  
+//          fetch(`${apiUrl}&search=${query}`)
+//              .then(response => response.json())
+//              .then(data => {
+//                  const resultsContainer = document.getElementById('results');
+//                  resultsContainer.innerHTML = '';  
+//                  data.results.forEach(item => {
+//                      const div = document.createElement('div');
+//                      div.classList.add('result-item');
+//                      div.innerHTML = `
+//                          <img src="${item.background_image}" alt="${item.name}">
+//                          <span>${item.name}</span>
+//                      `;
+//                      resultsContainer.appendChild(div);
+//                  });
+//              })
+//              .catch(error => console.error('Error:', error));
+//      } else {
+//          document.getElementById('results').innerHTML = '';  
+//      }
+//  });
 
