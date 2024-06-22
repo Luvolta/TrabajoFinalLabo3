835377e6515f6590786a865aebdec50ff7fec65c
Luvolta <luvolta99@gmail.com>
2024-06-22 14:08:34 -0300

    Creación auto Slider

--- FunZone/main.js
+++ FunZone/main.js
@@ -1,24 +1,36 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector('.next').addEventListener('click', () => {
+    changeSlide(currentSlide + 1);
+});
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector('.prev').addEventListener('click', () => {
+    changeSlide(currentSlide - 1);
+});
+
+function changeSlide(newSlide) {
+    slides[currentSlide].classList.remove('active');
+    
+    setTimeout(() => {
+        slides[currentSlide].style.display = 'none';
+        currentSlide = (newSlide + totalSlides) % totalSlides;
+        slides[currentSlide].style.display = 'block';
+        setTimeout(() => {
+            slides[currentSlide].classList.add('active');
+        }, 20);
+    }, 500); 
+}
+
+
+setInterval(() => {
+    changeSlide(currentSlide + 1);
+}, 5000);
+
+slides[currentSlide].style.display = 'block';
+setTimeout(() => {
+    slides[currentSlide].classList.add('active');
+}, 20);
+
+
